fed1e27d1fdf38adba6fd43edf15e8942e3cff0f
douniad <59784097+douniad@users.noreply.github.com>
2021-07-25 05:24:23 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    TOKEN_KEY: 'babble-client-auth-token',
+    API_ENDPOINT: 'https://git.heroku.com/intense-coast-03127.git',
+    TOKEN_KEY: process.env.REACT_APP_API_KEY,
   }
