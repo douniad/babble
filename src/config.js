6480ce74031d66893e466b36caca3079863f16f2
douniad <59784097+douniad@users.noreply.github.com>
2021-07-25 16:38:03 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://git.heroku.com/intense-coast-03127.git',
+    API_ENDPOINT: 'https://git.heroku.com/babble.git',
     TOKEN_KEY: process.env.REACT_APP_API_KEY,
   }
