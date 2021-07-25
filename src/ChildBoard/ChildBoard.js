4020b475e862b0e872d294d6bbf10fb24edd6b6b
Dounia <douniadif@outlook.com>
2021-07-25 17:29:32 +0200

    fixed

--- src/ChildBoard/ChildBoard.js
+++ src/ChildBoard/ChildBoard.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {NavLink} from 'react-router-dom'
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import config from '../config'
 import BabbleContext from '../BabbleContext'
@@ -8,54 +8,53 @@ import TokenService from '../Services/Token-Service'
 import '../baby.png'
 
 class ChildBoard extends Component {
-static contextType = BabbleContext
+    static contextType = BabbleContext
     state = {
         children: [],
         updates: []
     }
-    
+
     componentDidMount() {
-        fetch(config.API_ENDPOINT+'/children', {
+        fetch(config.API_ENDPOINT + '/children', {
             headers: {
                 'authorization': `Bearer ${TokenService.getAuthToken()}`,
             },
         })
-        .then(res => res.json())
-        .then((children = []) => {
-           if (children.error ) {
-               return
-           } 
-            this.context.setChildren(
-                children
-            )
-        })
-       
+            .then(res => res.json())
+            .then((children = []) => {
+                if (children.error) {
+                    return
+                }
+                this.context.setChildren(
+                    children
+                )
+            })
+
     }
     render() {
-    return(
-        <div className="childboard">
-        
-            <ul className="childrenlist">
-                {this.context.children.map(child =>
-                <li key= {child.id} className={`child-item ${this.props.selectedChild === child.name ? 'active-child' : ''}`}>
-                    <NavLink to={`/children/${child.id}`} className="childlink">
-                        <span><img className="baby" src="/static/media/baby.79330cd5.png" alt="Baby"/>{child.name}</span>
-                    </NavLink>
-                </li>
-                )
-                }
-            </ul>
-            
-       
-        <section className="buttonsection">
-        <NavLink className="linknoline" to={`/addchild`} ><button type="button" className="add-child">Add Child</button> </NavLink>
-           
-            <NavLink className="linknoline" to={`/addupdate`}> <button type="button" className="add-update">Add Update</button> </NavLink>
-            </section>
-        </div>
-    )
+        return (
+            <div className="childboard">
+
+                <ul className="childrenlist">
+                    {this.context.children.map(child =>
+                        <li key={child.id} className={`child-item ${this.props.selectedChild === child.name ? 'active-child' : ''}`}>
+                            <NavLink to={`/children/${child.id}`} className="childlink">
+                                <span><img className="baby" src="/static/media/baby.79330cd5.png" alt="Baby" />{child.name}</span>
+                            </NavLink>
+                        </li>
+                    )
+                    }
+                </ul>
+
+                <section className="buttonsection">
+                    <NavLink className="linknoline" to={`/addchild`} ><button type="button" className="add-child">Add Child</button> </NavLink>
+                    <NavLink className="linknoline" to={`/addupdate`}> <button type="button" className="add-update">Add Update</button> </NavLink>
+                </section>
+                
+            </div>
+        )
     }
-} 
+}
 
 export default ChildBoard
 
--- src/FirstPage/FirstPage.js
+++ src/FirstPage/FirstPage.js
@@ -1,24 +1,50 @@
-import React, {Component} from 'react'
-import {NavLink} from 'react-router-dom'
+import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 import './FirstPage.css'
+import '../moana.png'
+import TokenService from '../Services/Token-Service'
+import BabbleContext from '../BabbleContext'
+
+
+
 
 class FirstPage extends Component {
+
+    static contextType = BabbleContext
+
+    renderLoggedOutFirstPage() {
+        return <div className="firstpage">
+            <NavLink className="linknoline" to={'/register'}><button type="button" className="registerbutton">Register</button></NavLink>
+            <NavLink className="linknoline" to={'/login'}><button type="button" className="loginbutton">Log In</button></NavLink>
+        </div>
+    }
+
+    renderLoggedInFirstPage() {
+        <div></div>
+    }
+
+
     render() {
-        return(
-            <div className="landing-page">
-                
-                    <h1 className="babble-header">
+        return (
+
+            <div className="landingpage">
+                <section className="headersection">
+                    <h1 className="babbleheader">
                         babble
                     </h1>
-                <h2>All about your little ones.</h2>
-             
-           
-       <main className="firstpage">
-       <NavLink className="linknoline" to={'/register'}><button type="button" className="registerbutton">Register</button></NavLink> 
-       <NavLink className="linknoline" to={'/login'}><button type="button" className="loginbutton">Log In</button></NavLink> 
-       </main>
-      
-       </div> 
+                    <h2 className="h2babble">All about your little ones.</h2>
+                </section>
+
+                <span><img className="moana" src="/static/media/moana.6abbc0bd.png" alt="baby with turtle" /></span>
+
+
+                <main className="stuff">
+                    {TokenService.hasAuthToken()
+                        ? this.renderLoggedInFirstPage()
+                        : this.renderLoggedOutFirstPage()}
+                </main>
+
+            </div>
         )
     }
 }
