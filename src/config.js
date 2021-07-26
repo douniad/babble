964deec295a34e2b4fa7d5b8cd6461d2655f4cd1
Dounia <douniadif@outlook.com>
2021-07-26 17:08:45 +0200

    added variable

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://git.heroku.com/babble.git',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000/api',
     TOKEN_KEY: process.env.REACT_APP_API_KEY,
   }
